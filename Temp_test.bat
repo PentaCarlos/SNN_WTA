1338f2785ef0ab9d382168426b22a2cbc26201f2
CarlosJP <carl_9966@live.com>
2024-07-08 16:48:00 +0900

    Edit batch file routine for getting accuracy of each Temp file

--- Temp_test.bat
+++ Temp_test.bat
@@ -1,9 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 set Temp=Temp_It_
-set Rand=1
+set Rand=0
 
-FOR /L %%s IN (1000,1000,40000) DO (
+FOR /L %%s IN (1000,1000,5000) DO (
     set "c=!Temp!%%s"
-    python Validate.py -s !Rand! -f !c! -t "True" -m %%s
+    python Validate.py -s !Rand! -f !c! -t "True" -m %%s -gb "False" -n "True"
+)
+
+FOR /L %%s IN (10000,5000,40000) DO (
+    set "c=!Temp!%%s"
+    python Validate.py -s !Rand! -f !c! -t "True" -m %%s -gb "False" -n "True"
 )
