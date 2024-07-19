6c1880672f5ebb49f93372c8a65fe8e556cc8f84
CarlosJP <carl_9966@live.com>
2024-07-19 14:46:19 +0900

    Batch file added to run Validate.py with different random seeds

--- Validate_test.bat
+++ Validate_test.bat
@@ -4,6 +4,5 @@ set Seed=pairSTDP_S
 
 FOR %%s IN (10 31 41 68) DO (
     set "c=!Seed!%%s"
-    echo !c!
-    python Train.py -s %%s -f !c! -d 40000 -e 1
+    python Validate.py -s %%s -f !c! -m 40000
 )
