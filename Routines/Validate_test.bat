4593fa44e5e91cba0f2d1873bcb16ea7317088dc
CarlosJP <carl_9966@live.com>
2024-07-29 14:12:15 +0900

    Add subfolder for running batch routines

--- Routines/Validate_test.bat
+++ Routines/Validate_test.bat
@@ -2,8 +2,9 @@
 setlocal enabledelayedexpansion
 set Seed=pairSTDP_S
 
+cd ..
+
 FOR %%s IN (10 31 41 68) DO (
     set "c=!Seed!%%s"
-    echo !c!
-    python Train.py -s %%s -f !c! -d 40000 -e 1
+    python Validate.py -s %%s -f !c! -m 40000
 )
